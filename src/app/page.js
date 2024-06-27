a6d43cf462c71fb6d63830d9e2317b9732d7fb44
Ajaybalajiprasad <ajaybalajiprasad02@gmail.com>
2024-06-27 21:41:35 +0530

    Auto Redirect

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
 "use client"
-import React, { useState } from 'react';
-import styles from './page.module.css'; 
+import { useRouter } from 'next/navigation';
+import React from 'react';
+import styles from './page.module.css';
 
-const App = () => {
+export default function App() {
+  const router = useRouter();
   const link = 'http://65.0.72.48:3000/';
 
-
-  const handleClick = () => {
-    window.open(link, '_blank');
-  };
+  // Client-side redirection
+  if (typeof window !== "undefined") {
+    router.replace(link);
+    return null;
+  }
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Welcome to My File Sharing App!</h1>
-    
-      <button onClick={handleClick}  className={styles.shareButton}>Use the App!</button>
+      <h1 className={styles.heading}>Redirecting to the File Sharing App...</h1>
     </div>
   );
-};
-
-export default App;
+}
